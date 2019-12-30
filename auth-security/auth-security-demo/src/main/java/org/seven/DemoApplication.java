720ee9f97345d6b4d22fd1694a43af4e86843943
xuanwei.lin <xuanwei.lin@dianrong.com>
2019-12-30 22:10:11 +0800

    demo start success: /actuator/health

---
+++ auth-security/auth-security-demo/src/main/java/org/seven/DemoApplication.java
@@ -0,0 +1,16 @@
+package org.seven;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Seven.Lin
+ * @date 2019/12/30 20:54
+ */
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
---
+++ auth-security/auth-security-demo/src/main/java/org/seven/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package org.seven.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Seven.Lin
+ * @date 2019/12/30 21:49
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll()
+                .and().logout().permitAll();
+    }
+}
